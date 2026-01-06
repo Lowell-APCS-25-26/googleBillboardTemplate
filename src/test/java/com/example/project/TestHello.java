0773b860bc0a1b4b789a80f373cc02e97102f25f
mrchanr <42151767+mrchanr@users.noreply.github.com>
2026-01-06 08:41:24 -0800

    Update TestHello.java

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -18,7 +18,7 @@ public class TestHello {
      System.setOut(new PrintStream(bos));
 
      // action
-     TestHello.main(null);
+     //TestHello.main(null);
 
      // assertion
      assertEquals("7.427466391E9\n", bos.toString()); 
